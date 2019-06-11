c56bfc95b9b6e22b6d342d3fde7814e42e2bc71c
Anton Kozyr <anton.kozyr@globallogic.com>
2019-06-11 14:33:34 +0300

    * Add express routing to the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import express from 'express'
+import mainRoutes from './routes/main.js'
 
 const PORT = process.env.EXPOSED_PORT
 const HOST = process.env.HOST
 
 const app = express()
-app.get('/', (req, res) => {
-  res.send('Hello world\n')
-})
-
+app.use('/', mainRoutes)
 app.listen(PORT, HOST)
+
 console.log(`Running on http://${HOST}:${PORT}`)
