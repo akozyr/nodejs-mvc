6a825f904d8f326ddaa302a07127635dcddccb31
Anton Kozyr <anton.kozyr@globallogic.com>
2019-06-12 14:17:44 +0300

    * Add puppeteer package and a test example for it

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,5 +1,18 @@
+import puppeteer from 'puppeteer'
+
+async function getPic() {
+  const browser = await puppeteer.launch({args: ['--no-sandbox']})
+  const page = await browser.newPage()
+  await page.goto('https://google.com')
+  await page.screenshot({path: 'google.png'})
+
+  await browser.close()
+}
+
 export default {
   index (req, res) {
+    getPic()
+
     res.render('index', { title: 'Main - index', message: 'Hello there!!' })
   }
 }
