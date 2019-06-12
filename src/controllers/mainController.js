3654da6c166bdb07a5795d2a0c3b97111920a7d0
Anton Kozyr <anton.kozyr@globallogic.com>
2019-06-12 14:43:34 +0300

    * Scrape the generated json object from the target site

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,17 +1,26 @@
 import puppeteer from 'puppeteer'
 
-async function getPic() {
+async function scrape() {
   const browser = await puppeteer.launch({args: ['--no-sandbox']})
   const page = await browser.newPage()
-  await page.goto('https://google.com')
-  await page.screenshot({path: 'google.png'})
+  await page.goto('https://www.json-generator.com/')
 
-  await browser.close()
+  await page.click('#generate')
+  await page.waitFor(300)
+
+  const result = await page.evaluate(() => {
+    const json = document.querySelector('a#copy-code').getAttribute('data-clipboard-text');
+
+    return json
+  });
+
+  browser.close()
+  return result
 }
 
 export default {
   index (req, res) {
-    getPic()
+    scrape().then((res) => { console.log(res) })
 
     res.render('index', { title: 'Main - index', message: 'Hello there!!' })
   }
