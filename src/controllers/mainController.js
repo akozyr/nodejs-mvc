cf4f1099864ef1eb94e2df198a9c876b811752c8
Anton Kozyr <anton.kozyr@globallogic.com>
2019-06-13 11:48:48 +0300

    * Add package for dummy json generating

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,23 +1,28 @@
+import dummyJson from 'dummy-json'
+
 function getJsonSchema() {
-  return [
-    '{{repeat(5, 20)}}',
+  return `[
+    {{#repeat 20}}
     {
-      product_id: '{{objectId()}}',
-      isAvailable: '{{bool()}}',
-      price: '{{floating(100, 10000, 2)}}',
-      title: '{{lorem(10, "words")}}',
-      description: '{{lorem(2, "paragraphs")}}',
-      seller_name: '{{firstName()}} {{surname()}}',
-      company: '{{company().toUpperCase()}}',
-      email: '{{email()}}',
-      phone: '+1 {{phone()}}',
-      address: '{{integer(100, 999)}} {{street()}}, {{city()}}, {{state()}}, {{integer(100, 10000)}}'
+      "product_id": {{@index}},
+      "isAvailable": {{boolean}},
+      "price": {{float 100 10000 '0.00'}},
+      "title": "{{lorem 10}}",
+      "description": "{{lorem 250}}",
+      "seller_name": "{{firstName}} {{lastName}}",
+      "company": "{{company}}",
+      "email": "{{email}}",
+      "phone": "{{phone "+12 (x) xxx xxxx"}}",
+      "address": "{{int 1 100}} {{street}}"
     }
-  ]
+    {{/repeat}}
+  ]`
 }
 
 export default {
   index (req, res) {
+    res.send(dummyJson.parse(getJsonSchema()))
+
     res.render('index', { title: 'Main - index', message: 'Hello there!!' })
   }
 }
