94ddea1cbb18bcb1844b99a3fb44619814e03831
Anton Kozyr <anton.kozyr@globallogic.com>
2019-06-18 13:19:02 +0300

    * Update delays logic between requests * Create a service class for executing scraping

---
+++ src/services/GoogleImagesScraper.js
@@ -0,0 +1,51 @@
+import puppeteer from 'puppeteer'
+import path from 'path'
+
+export default class GoogleImagesScraper
+{
+  async receiveTags ()
+  {
+    const browser = await puppeteer.launch({args: ['--no-sandbox']})
+    const page = await browser.newPage()  
+    
+    const TARGET_URL = 'https://www.google.com/imghp'
+    await page.goto(TARGET_URL)
+
+    const SEARCH_BY_IMAGE_ICON_ELEMENT = 'div[jscontroller="TJw5qb"]'
+    await page.click(SEARCH_BY_IMAGE_ICON_ELEMENT)
+    
+    const UPLOAD_AN_IMAGE_TAB_ELEMENT = 'div#qbug > div > a'
+    await page.waitFor(UPLOAD_AN_IMAGE_TAB_ELEMENT)
+    await page.click(UPLOAD_AN_IMAGE_TAB_ELEMENT)
+
+    const IMAGE_INPUT_ELEMENT = 'input#qbfile'
+    const input = await page.$(IMAGE_INPUT_ELEMENT)
+    const filePath = path.normalize(path.join(__dirname, '../../car.jpg'))
+    input.uploadFile(filePath)
+
+    const ALL_TAB_ELEMENT = 'div#hdtb-msb-vis > div > a'
+    await page.waitFor(ALL_TAB_ELEMENT)
+    await page.click(ALL_TAB_ELEMENT)
+
+    const IMAGES_TAB_ELEMENT = 'div.hdtb-imb > a'
+    await page.waitFor(IMAGES_TAB_ELEMENT)
+    await page.click(IMAGES_TAB_ELEMENT)
+
+    const TAGS_CONTAINER_ID = 'isr_chc'
+    await page.waitFor(`#${TAGS_CONTAINER_ID}`)
+
+    const tags = await page.evaluate((tagsContainerId) => {
+      let tags = []
+      const tagsHtmlCollection = document.getElementById(tagsContainerId).childNodes[0].childNodes[0].children
+
+      for (item of tagsHtmlCollection) {
+        tags.push(item.text)
+      }
+
+      return tags
+    }, TAGS_CONTAINER_ID)
+
+    await browser.close()
+    return tags
+  }
+}
